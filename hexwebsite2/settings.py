b5a776c504572e1bd92de032269f63075ad5a053
kasra_samareh <kasragol80@yahoo.com>
2020-03-09 17:11:00 +0330

    new local settings

--- hexwebsite2/settings.py
+++ hexwebsite2/settings.py
@@ -123,3 +123,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+try :
+    from .local_settings import *
+except ImportError:
+    print("Looks like no Local file. You must be on production")
